34a251688e287c7523f024bb8bd154c4634ce88f
octoccoper <octocopper@gmail.com>
2019-02-20 19:19:19 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 class Node {
-  constructor() {
+  constructor(value, next) {
     this.value = value, // Some data that we stored in the link element
-      this.next = next // Pointer to the next element
+    this.next = next // Pointer to the next element
   }
 }
 
 class linkedList {
-  constructor() {
+  constructor(head) {
     this.head = null // Pointer to the first element in the linked list
   }
 
-  add() {
-    let node = new Node(value);
-
+  add(value) {
+    let node = new Node(value),
+      currentNode = this.head;
+    
     // If we have empty list
-    if (!node) { 
+    if (!currentNode) {
       this.head = node;
+      return node;
     }
 
+    // If list doesn't empty
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
 
+    currentNode.next = node;
+
+    return node;
   }
 }
